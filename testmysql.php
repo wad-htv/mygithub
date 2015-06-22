c13987890e73ab8236488ff1d0121433d5b0f436
wad-htv <wad@htv.co.th>
2015-06-22 08:45:06 +0700

    add PDO Connection String

--- testmysql.php
+++ testmysql.php
@@ -4,4 +4,8 @@ if (!$link) {
 	die('Could not connect to MySQL: ' . mysql_error()); 
 } 
 echo 'Connection OK'; mysql_close($link); 
+
+//PDO Connection
+$conn = new PDO( "sqlsrv:Server=10.1.0.9\MSSQLSRV ; Database = HTV2015 ", "sa", "$123root$", array(PDO::SQLSRV_ATTR_DIRECT_QUERY => true)); 
+
 ?> 
